f1283c03cdd2f815c25b9c5967293286a235810e
Tobias Wochinger <tobias.wochinger@student.htw-berlin.de>
2016-12-07 02:06:18 +0100

    updated for respiration sensor

--- YunBitalino/YunBitalino.ino
+++ YunBitalino/YunBitalino.ino
@@ -1,15 +1,17 @@
 #include <LinkedList.h>
 #include <Bridge.h>
 #include <Process.h>
-#include "Bandpass.h"
+#include "Filter.h"
 
 #define DEBUG false
 
 #define PIN_OF_ECG 0
-#define ECG_THRESHOLD 130
+#define ECG_THRESHOLD (-100)
+#define MIN_HEART_BEAT_INTERVAL 200
 
 #define PIN_OF_RESPIRATION 1
-#define RESPIRATION_THRESHOLD 200
+#define RESPIRATION_THRESHOLD 380
+#define MIN_BREATH_INTERVAL 400
 
 #define MINUTE_IN_MS 60000.0
 #define TIME_WINDOW_IN_MS 10000
@@ -19,84 +21,97 @@
 #define FIRST(list) (list->get(0))
 #define LAST(list) (list->get(list->size() - 1))
 
+#define INTERVAL_BIG_ENOUGH(list, minInterval) (timeOfMeasurement - LAST(list) >= minInterval)
+
 LinkedList<unsigned long> ecgPeaks;
+
 LinkedList<unsigned long> breathPeaks;
+boolean stillInOldBreathPeak = true;
 
-BandpassFilter filter;
-
-RespirationFilter respFilter;
+// Filters to clear input
+ECGFilter ecgfilter;
+RespirationFilter breathFilter;
 
 Process mqtt;
-unsigned long now;
+unsigned long timeOfMeasurement;
 unsigned long lastPrint;
+
 int ecgValue;
 int breathValue;
 
 void setup() {
   Serial.begin(57600);
-  //Bridge.begin();
- // mqtt.runShellCommandAsynchronously("python /root/yun_client.py");
+  Bridge.begin();
+  mqtt.runShellCommandAsynchronously("python /root/yun_client.py");
 }
 
 void loop() {
-  
-  now = millis(); // Here or one line down?
+  timeOfMeasurement = millis();
   
   ecgValue = analogRead(PIN_OF_ECG);
-  ecgValue = filter.step(ecgValue);
+  ecgValue = ecgfilter.step(ecgValue);
 
   breathValue = analogRead(PIN_OF_RESPIRATION);
+  breathValue = breathFilter.step(breathValue);
   
-  //ecgValue = (ecgValue / 1024.0 - 0.5) * 100;
-  
-  // Apply filter
-  //ecgValue = respFilter.step(ecgValue);
-
   if (DEBUG) {
-    Serial.print(now);
+    Serial.print(timeOfMeasurement);
     Serial.print(": ");
-    Serial.println(ecgValue);
-    
+    Serial.println(breathValue);   
   } else {
-    checkPeak(ecgValue, &ecgPeaks, ECG_THRESHOLD);
-    //checkPeak(breathValue, &breathPeaks, RESPIRATION_THRESHOLD);
+    checkECGPeak(ecgValue);
+    checkBreathPeak(breathValue);
     
     // Print bpm in regular intervalls
-    if (now - lastPrint > PRINT_INTERVALL_IN_MS) {
-      lastPrint = now;
+    if (timeOfMeasurement - lastPrint > PRINT_INTERVALL_IN_MS) {
+      lastPrint = timeOfMeasurement;
       int current = peaksPerMinute(&ecgPeaks);
       Serial.print("BPM: ");
       Serial.println(current);
       Bridge.put("bpm", String(current));
 
-//      current = peaksPerMinute(&breathPeaks);
-//      Serial.print("Breath: ");
-//      Serial.println(current);
-//      Bridge.put("respiration", String(current));
+      current = peaksPerMinute(&breathPeaks);
+      Serial.print("Breath: ");
+      Serial.println(current);
+      Bridge.put("respiration", String(current));
     }
   }
 }
 
-void checkPeak(int value, LinkedList<unsigned long>* list, int threshold) {
+void checkECGPeak(int value) {
    // Check whether it is a peak?
-    if (value >= threshold && now - LAST(list) >= 200) {
-      list->add(now);
-      // Remove peaks which are not in the time windows anymore
-      removeTooOldPeaks(list);
-    }
+    if (value <= ECG_THRESHOLD && INTERVAL_BIG_ENOUGH((&ecgPeaks), MIN_HEART_BEAT_INTERVAL)) {
+      refreshList(&ecgPeaks, value);
+    } 
+}
+
+void checkBreathPeak(int value) {
+  if (value > RESPIRATION_THRESHOLD && INTERVAL_BIG_ENOUGH((&breathPeaks), MIN_BREATH_INTERVAL) && !stillInOldBreathPeak) {
+    stillInOldBreathPeak = true;
+    refreshList(&breathPeaks, value);
+  } else if (value < 350) {
+    stillInOldBreathPeak = false;
+  }
+}
+
+void refreshList(LinkedList<unsigned long>* list, int value) {
+  list->add(timeOfMeasurement);
+  removeTooOldPeaks(list);
 }
 
 void removeTooOldPeaks(LinkedList<unsigned long>* list) {
-  while (list->size() > 0 && now - list->get(0) > TIME_WINDOW_IN_MS) {
+  while (list->size() > 0 && (timeOfMeasurement - (list->get(0))) >= TIME_WINDOW_IN_MS) {
     list->shift();
   }
 }
 
 int peaksPerMinute(LinkedList<unsigned long>* list) {
   if (list->size() > 1) {
-      return list->size() * (MINUTE_IN_MS / (LAST(list) - FIRST(list)));
+      return list->size() * (MINUTE_IN_MS / (timeOfMeasurement - FIRST(list)));
   } else {
     return -1;
   }
 }
+
+
 
